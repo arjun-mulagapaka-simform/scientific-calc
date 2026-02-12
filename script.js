b209742b8c1232bd49e1c1cceff899678ec8ca24
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-02-12 15:42:08 +0530

    minor changes + modules listing

---
+++ script.js
@@ -0,0 +1,21 @@
+// memory module
+// I/O module
+// exception handling module
+// calculations:
+// -> square
+// -> square root
+// -> inverse
+// -> abs
+// -> user input exponentiation
+// -> mod
+// -> square root
+// -> factorial
+// -> division
+// -> multiplication
+// -> subtraction
+// -> addition
+// -> natural log (with e)
+// -> log with base 10
+
+
+// data structures: stack for inputs, stack for memory module

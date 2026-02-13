072ad7ac844cc74fe478bc03077de4aa44919ce6
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-02-13 11:28:33 +0530

    Added functions for input manipulation

--- IOops.js
+++ IOops.js
@@ -21,4 +21,30 @@ export class IOops{
     static setData(num){
         inp.value = num;
     }
+    static append(e){
+        let digit = IOops.getDataOp(e);
+        inp.value += digit;
+    }
+    static clear(){
+        inp.value = "";
+    }
+    static pop(){
+        if (inp.value === "") return;
+        inp.value = inp.value.slice(0,-1);
+    }
+    static addDecimal(){
+        const val = inp.value;
+        const lastNumber = val.split(/[\+\-\*\/\(\)]/).pop();
+
+        if (!lastNumber.includes(".")) {
+            inp.value += ".";
+        }
+    }
+    static toggleSign(){
+        if (inp.value === "") IOops.append('-');
+        let sign = inp.value.slice(1);
+        if (!sign.includes('-')){
+            IOops.append('-');
+        }
+    }
 }
